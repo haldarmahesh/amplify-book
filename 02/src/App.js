522ce13cf85f67b4d586042bdbdaff3ac3927775
Mahesh Haldar <haldar.mahesh@gmail.com>
2023-07-03 23:39:40 +0530

    Add chapter 02

---
+++ 02/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+import HeaderStrip from './components/HeaderStrip';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderStrip/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
